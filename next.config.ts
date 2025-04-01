4437c39c09b730e0542898ee330ea1bda9662384
tttol <tkhstol.929@gmail.com>
2025-04-02 06:52:09 +0900

    disable image optimization

--- next.config.ts
+++ next.config.ts
@@ -3,6 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   output: "export", // static site generation(SSG)
+  images: { unoptimized: true },
 };
 
 export default nextConfig;
